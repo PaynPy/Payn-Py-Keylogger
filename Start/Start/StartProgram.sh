4af57586a8a64beca0d1e794b5d308665a4efdb5
PaynPy <69658419+PaynPy@users.noreply.github.com>
2020-08-13 21:34:49 -0400

    Delete StartProgram.sh

--- Start/Start/StartProgram.sh
+++
@@ -1,4 +0,0 @@
-user=$USER
-echo "<Payn Py Message> You need to put the program folder into documents or it wont work!"
-python3 ~/Documents/PaynPy\ Keylogger\ V2/Code/paynpykeylogger.py
-./~/Documents/PaynPy\ Keylogger\ V2/Code/FindProcessRunning.sh
