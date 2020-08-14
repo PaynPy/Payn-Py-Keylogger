cc6b72caea1c4e035b6c91c114d48aee4c6e6660
PaynPy <69658419+PaynPy@users.noreply.github.com>
2020-08-14 09:29:59 -0400

    Update StartProgram.sh

--- Start/StartProgram.sh
+++ Start/StartProgram.sh
@@ -1,4 +1,4 @@
 user=$USER
 echo "<Payn Py Message> You need to put the program folder into documents or it wont work!"
 python3 ~/Documents/Payn-Py-Keylogger-master/Code/paynpykeylogger.py
-./~/Documents/Payn-Py-Keylogger-master/Code/FindProcessRunning.sh
+./~/Documents/Payn-Py-Keylogger/Code/FindProcessRunning.sh
